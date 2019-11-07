1b5dcbe8fe949a1740974c1a3b9ee5ac6380ffd6
MRoci <mroci@bruttocarattere.org>
2019-11-07 17:14:58 +0100

    symspell: add lookup_compound test

--- src/symspell.rs
+++ src/symspell.rs
@@ -696,3 +696,72 @@ impl<T: StringStrategy> SymSpell<T> {
             .collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use string_strategy::UnicodeiStringStrategy;
+
+    #[test]
+    fn test_lookup_compound() {
+        let edit_distance_max = 2;
+        let mut sym_spell = SymSpell::<UnicodeiStringStrategy>::default();
+        sym_spell.load_dictionary("./data/frequency_dictionary_en_82_765.txt", 0, 1, " ");
+
+        let typo = "whereis th elove";
+        let correction = "whereas the love";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(2, results[0].distance);
+        assert_eq!(64, results[0].count);
+
+        let typo = "the bigjest playrs";
+        let correction = "the biggest players";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(2, results[0].distance);
+        assert_eq!(34, results[0].count);
+
+        let typo = "Can yu readthis";
+        let correction = "can you read this";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(3, results[0].distance);
+        assert_eq!(3, results[0].count);
+
+        let typo = "whereis th elove hehad dated forImuch of thepast who couqdn'tread in sixthgrade and ins pired him";
+        let correction = "whereas the love head dated for much of the past who couldn't read in sixth grade and inspired him";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(9, results[0].distance);
+        assert_eq!(0, results[0].count);
+
+        let typo = "in te dhird qarter oflast jear he hadlearned ofca sekretplan";
+        let correction = "in the third quarter of last year he had learned of a secret plan";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(9, results[0].distance);
+        assert_eq!(0, results[0].count);
+
+        let typo = "the bigjest playrs in te strogsommer film slatew ith plety of funn";
+        let correction = "the biggest players in the strong summer film slate with plenty of fun";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(9, results[0].distance);
+        assert_eq!(0, results[0].count);
+
+        let typo = "Can yu readthis messa ge despite thehorible sppelingmsitakes";
+        let correction = "can you read this message despite the horrible spelling mistakes";
+        let results = sym_spell.lookup_compound(typo, edit_distance_max);
+        assert_eq!(1, results.len());
+        assert_eq!(correction, results[0].term);
+        assert_eq!(10, results[0].distance);
+        assert_eq!(0, results[0].count);
+    }
+}
